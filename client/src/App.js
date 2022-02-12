6c5cfd9a938da6aa0d56eb8672c89274b58f5472
Laura Cole <lauracole1900@comcast.net>
2022-02-11 22:19:56 -0700

    updated react dependencies; added Apollo Cache code to App.js; updated LoginForm with gql mutation

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,53 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from "@apollo/client/link/context";
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+
+//=====================//
+//      Functions      //
+//=====================//
+
+// Creates Apollo Client http link for GraphQL operations
+const httpLink = createHttpLink({
+  uri: "/graphql"
+});
+
+// Sets authentication into context
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  }
+});
+
+// Instantiates the client object and the cache object with some specific options
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache({})
+});
+
+
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={<SearchBooks />} />
+            <Route exact path='/saved' element={<SavedBooks />} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,13 +2,16 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
-import Auth from '../utils/auth';
+import { useMutation } from "@apollo/client";
+import { LOGIN } from "../utils/gql";
+import Auth from "../utils/auth";
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  const [login, { error }] = useMutation(LOGIN);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,15 +29,13 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
+      const { data } = await login({
+        variables: { ...userData },
+      });
       if (!response.ok) {
-        throw new Error('something went wrong!');
+        throw new Error('Incorrect login. Try again');
       }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
