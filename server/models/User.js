80fca49089b44b024c680833964b72bdf6123d06
Laura Cole <lauracole1900@comcast.net>
2022-02-12 19:41:41 -0700

    added bookCount meta field on User model

--- server/models/User.js
+++ server/models/User.js
@@ -21,6 +21,9 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    meta: {
+      bookCount: Number
+    },
     // set savedBooks to be an array of data that adheres to the bookSchema
     savedBooks: [bookSchema],
   },
